2317c3b36d41140696afa4cdad2017db6dd44c6e
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-04 11:11:15 -0300

    arrays ds commented

--- Data Structures/Arrays/arrays_ds.py
+++ Data Structures/Arrays/arrays_ds.py
@@ -3,11 +3,17 @@
 
 import sys
 
+# Get all user inputs
 n = int(raw_input().strip())
 arr = map(int,raw_input().strip().split(' '))
+
+# Reverse array
 arr.reverse()
+
+# Initializes output string variable
 result = ""
 
+# Concatenates all values in output string
 for value in arr:
     result += "{} ".format(value)
 
