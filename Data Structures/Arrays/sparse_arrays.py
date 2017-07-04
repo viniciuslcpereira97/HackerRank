f9b56144e81ea1995650f0e867d825baf353e4ed
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-04 11:11:42 -0300

    sparse arrays algorithm commented

--- Data Structures/Arrays/sparse_arrays.py
+++ Data Structures/Arrays/sparse_arrays.py
@@ -3,10 +3,13 @@
 
 import re
 
+# Search by pattern in given string
 def get_occurrences(query, string):
     pattern = re.compile( r"\b({})\b".format(query) )
     return pattern.findall(string)
 
+# Get the number of strings that will be inputed
+# Get all strings and provide all strings concatenated
 def get_string():
     N_strings = int( raw_input() )
     concat_strings = ""
@@ -15,6 +18,8 @@ def get_string():
 
     return concat_strings
 
+# Get the number of patterns
+# Return a list of patterns
 def get_queries():    
     N_queries = int( raw_input() )
     queries = []
@@ -27,7 +32,7 @@ def get_queries():
 string = get_string()
 queries = get_queries()
 
+# For each query in queries list search by the pattern in string
 # O(n)
-
 for query in queries:
     print len( get_occurrences( query, string ) )
