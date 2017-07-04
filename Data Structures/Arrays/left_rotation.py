50df2f1fd0a75d728d9598badd78cd901ea0f524
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-04 11:11:29 -0300

    left rotation algorithm commented

--- Data Structures/Arrays/left_rotation.py
+++ Data Structures/Arrays/left_rotation.py
@@ -1,16 +1,23 @@
 #!usr/bin/env python
 #-*- coding: utf-8 -*-
 
+# User inputs
 u_input = raw_input().split()
 arr_input = raw_input()
+
+# Parses user inputs
 arr_len = int( u_input[0] )
 arr_rot = int( u_input[1] )
 
+# Get array values
 arr = arr_input.split()
+
+# Creates empty array
 new_arr = [None] * arr_len
 
+# Checks value of new index to each value
+# Rotates each value to new index
 # O(n)
-
 for x, i in enumerate(arr):
     rotation = ( x - arr_rot ) if ( x - arr_rot ) >= 0 else ((arr_len) + (x - arr_rot)) 
     new_arr[rotation] = i
