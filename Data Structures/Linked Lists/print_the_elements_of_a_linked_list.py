4b07aff4bb186a29be2700a58f0b2047fc7389ef
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-04 11:31:55 -0300

    print the elements of a linked list algorithm

---
+++ Data Structures/Linked Lists/print_the_elements_of_a_linked_list.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+"""
+ Print elements of a linked list on console
+ head input could be None as well for empty list
+ Node is defined as
+"""
+
+def print_list(head):
+    while head != None:
+        print head.data
+        head = head.next
+
+class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
