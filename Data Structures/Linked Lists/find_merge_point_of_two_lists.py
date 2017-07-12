9d70dcf0c6ab4dbf1083f9da42ba358ad9d50422
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-12 16:00:11 -0300

    trash removed from find merge point of two lists algorithm

--- Data Structures/Linked Lists/find_merge_point_of_two_lists.py
+++ Data Structures/Linked Lists/find_merge_point_of_two_lists.py
@@ -1,8 +1,6 @@
 #!usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from print_the_elements_of_a_linked_list import print_list
-
 """
  Find the node at which both lists merge and return the data of that node.
  head could be None as well for empty list
@@ -26,12 +24,3 @@ def FindMergeNode(headA, headB):
             return headB.data
         else:
             headB = headB.next
-
-headA = Node(2, Node(4, Node(3, Node(7))))
-headB = Node(6, Node(11, Node(5, Node(3, Node(7)))))
-print FindMergeNode(headA, headB)
-""" TEST CASE
-    2-4-3-5-7 --> 6-11-3-5-7
-    2-4-3-7 --> 6-11-5-3-7
-    2-4-7 --> 6-11-5-3-7
-"""
