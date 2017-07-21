79ef734783789842b53f6a4b95dae601b2491a5a
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-21 13:48:12 -0300

    preorder traversal algorithm trash removed

--- Data Structures/Trees/preorder_traversal.py
+++ Data Structures/Trees/preorder_traversal.py
@@ -15,24 +15,9 @@ class Node(object):
         self.left = left
         self.right = right
 
-n1 = Node(1)
-n2 = Node(2)
-n3 = Node(3)
-n4 = Node(4)
-n5 = Node(5)
-n6 = Node(6)
-n1.right = n2
-n2.right = n5
-n5.left = n3
-n3.right = n4
-n4.right = n6
-empty_head = n1
-
 def preOrder(root):
     if not root:
         return
     print root.data,
     preOrder(root.left)
     preOrder(root.right)
-
-preOrder(empty_head)
