7128a8c1d519c8ef626140f65ed71ffbc85d2b40
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-10 15:22:14 -0300

    matching specific string regex

---
+++ Regex/matching_specific_string.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import re
+
+Test_String = raw_input()
+Regex_Pattern = r'hackerrank'    # Do not delete 'r'.
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print "Number of matches :", len(match)
