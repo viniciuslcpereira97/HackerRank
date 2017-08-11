27425d4cc61a58c3c2584d3f8d58fe0044ac600b
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-11 11:49:16 -0300

    bot saves princess AI algorithm

---
+++ Artificial Intelligence/bot_saves_princess.py
@@ -0,0 +1,55 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+moves = {
+    "Y" : {"NEG": "UP", "POS": "DOWN"},
+    "X" : {"NEG": "LEFT", "POS": "RIGHT"} 
+}
+
+def drawGrid():
+    m = input()
+    grid = []
+    
+    for i in xrange(0, m):
+        grid.append( list(raw_input()) )
+
+    return grid
+
+def getEntitiesPosition(grid):
+    bot_pos = (len(grid) - 1) / 2
+    bot_pos = (bot_pos, bot_pos)
+
+    for n, x in enumerate(grid):
+         if 'p' in x:
+            peach = (n, x.index('p'))
+
+    return [bot_pos, peach]
+
+def getCommand(n1, n2, axis):
+    global moves
+    sub = "NEU"
+    command_num = n1 - n2
+    commands = []
+    if n1 < n2:
+        sub = "NEG"
+        command_num = command_num * -1
+    elif n1 > n2:
+        sub = "POS"
+
+    for x in range(0, command_num):
+        commands.append( moves[axis][sub] )
+
+    return commands
+
+
+def displayPathtoPrincess(positions):
+    x1, y1 = positions[0]
+    x2, y2 = positions[1]
+    x_commands = getCommand(x1, x2, "X")
+    y_commands = getCommand(y1, y2, "Y")
+    all_commands = x_commands + y_commands
+    print "\n".join(all_commands)
+
+grid = drawGrid()
+positions = getEntitiesPosition(grid)
+displayPathtoPrincess(positions)
