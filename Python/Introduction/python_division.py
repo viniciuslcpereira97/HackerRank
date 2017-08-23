f405a5495b0256e1d6efc0ad9acabc318cba5247
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-23 15:14:42 -0300

    python division algorithm

---
+++ Python/Introduction/python_division.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from __future__ import division
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    
+    print a // b
+    print a / b
